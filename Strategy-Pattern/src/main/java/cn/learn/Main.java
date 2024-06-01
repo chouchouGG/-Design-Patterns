08f9e8eae814e3155368d02e60376878de943db8
chouchouGG <983897884@qq.com>
2024-06-02 04:59:18 +1000

    feat: 创建设计模式学习项目

---
+++ Strategy-Pattern/src/main/java/cn/learn/Main.java
@@ -0,0 +1,13 @@
+package cn.learn;
+
+/**
+ * @program: Default (Template) Project
+ * @description: ${description}
+ * @author: chouchouGG
+ * @create: 2024-06-02 04:45
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
